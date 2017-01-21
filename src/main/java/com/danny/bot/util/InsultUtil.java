7e52421ce3823cc62918f5985ffc488da9abb4c5
Tiffany <Tiffany@TiffanysPC>
2017-01-20 23:49:15 -0500

    Added comments to insults.

--- src/main/java/com/danny/bot/util/InsultUtil.java
+++ src/main/java/com/danny/bot/util/InsultUtil.java
@@ -23,23 +23,23 @@ public final class InsultUtil {
 		int randNum = random.nextInt(9);
 		switch(randNum) {
 		case 0:
-			return name + ", i saw that";
+			return name + ", i saw that"; //Danny
 		case 1:
-			return name + " why you so poop";
+			return name + " why you so poop"; //Danny
 		case 2:
-			return name + ", my cat can play better than you";
+			return name + ", my cat can play better than you"; //Danny
 		case 3:
-			return name + " you so fat you has to buy two airplane tickets";
+			return name + " you so fat you has to buy two airplane tickets"; //Danny
 		case 4:
-			return name + " loses with infinite lives";
+			return name + " loses with infinite lives"; //Danny
 		case 5:
-			return name + " cant aim cant shoot";
+			return name + " cant aim cant shoot"; //Danny
 		case 6:
-			return name + " needs to buy a kit";
+			return name + " needs to buy a kit"; //Danny
 		case 7:
-			return name + " needs help";
+			return name + " needs help"; //Tiffany
 		case 8:
-			return "someone should call a doctor, " + name + " has a severe burn";
+			return "someone should call a doctor, " + name + " has a severe burn"; //Tiffany
 		default:
 			return name + " likes cows";
 			
